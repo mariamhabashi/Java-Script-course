4486e14c5113ac866d1d07ca154f128c2a8fa795
Mariam El-Habashi <CDS.MariamMahmoud23340@alexu.edu>
2025-01-23 22:23:30 +0200

    Error in console

---
+++ Introduction/Error Check/script.js
@@ -0,0 +1,22 @@
+document.write("<p>test</p>");
+document.write("<p>test</p>");
+document.wrte("<p>test</p>"); //error
+//Right click > inspect >> console
+//F12 
+/* Error msg in the console :
+script.js:3
+ Uncaught TypeError: document.wrte is not a function
+    at script.js:3:10 
+*/
+
+docment.write("<p>test</p>"); //error
+/* Error msg in the console : 
+script.js:3 
+ Uncaught TypeError: document.wrte is not a function
+    at script.js:3:10
+*/
+document.write("<p>test</p>");
+
+/* Js is automatic semicolon insertion >> but you should put each code in a new line or put it manually */
+
+
