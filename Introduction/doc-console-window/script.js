c6ee0cb6db47886f5850384a969872462e01d905
Mariam El-Habashi <CDS.MariamMahmoud23340@alexu.edu>
2025-01-23 19:50:31 +0200

    Important Comparison

---
+++ Introduction/doc-console-window/script.js
@@ -0,0 +1,36 @@
+//To control the html file we use document. (the html file is a document)
+document.write('hello world');  // add hello world in the html page
+document.querySelector('h1').style.color = 'red'; // change the color of the h1 in the html file
+
+/*
+To write in the console we use console. <<object
+ايه فايدة ال 
+console
+هنا انا بجرب الكود بشكل فوري يعني لو مش عايزة ابوظ ال 
+source code 
+ف بروح اجربه الاول في ال 
+console 
+اشوف بيعمل ايه بعدين اروح اجربه في ال 
+html
+*/
+console.log('hello world'); //write in the console section (won't show in the html page)
+console.error('error'); //error msg
+
+/*Control the browser<<object we use window. 
+window contains all the information and functions of the browser and all javascript instructions 
+write in the alert box in the browser */
+window.alert("alert box");
+window.print(); // will print the page or download as pdf
+
+/*ال console
+ window جوا ال 
+consoleف لو انا بكتب في ال 
+ مش لازم اكتب 
+ window.console.log('hello world'); هتشتغل عادي
+ console.error('error'); هتشتغل عادي
+ print() هتشتغل عادي
+ لإن هو عارف اني كدة كدة جوا ال window 
+ مش محتاج اعرفه واكتبها ف بستخدم اي instruction 
+ من window 
+ consoleجوا ال 
+*/
